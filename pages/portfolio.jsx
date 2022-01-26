b82daaa1631d001245d4035aefc01adddec202a0
h8m0ss <danarmenta32@gmail.com>
2022-01-25 21:54:37 -0600

    added basic portfolio

--- pages/portfolio.jsx
+++ pages/portfolio.jsx
@@ -1,5 +1,24 @@
-export default function Portfolio() {
+import { server } from '../config';
+
+export default function Portfolio({ projects }) {
+
+    const projectComponents = projects.map((v) => <div>{v.title}</div>);
+
     return (
-        <h1>Hello</h1>
+        <div className="h-screen w-screen flex">
+            <div className="flex m-20 flex-grow rounded-3xl bg-gray-300 shadow-xl">
+                {projectComponents}
+            </div>
+        </div>
     );
+}
+
+export async function getStaticProps(context) {
+
+    const res = await fetch(`${server}/api/projects`)
+    const data = await res.json()
+
+    return {
+        props: { projects: data },
+    }
 }
