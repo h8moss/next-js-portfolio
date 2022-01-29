05c20fd595bb5d80a68b2160df4cf1185b5627f5
h8m0ss <danarmenta32@gmail.com>
2022-01-29 00:18:32 -0600

    changed project into a class

--- pages/api/projects.ts
+++ pages/api/projects.ts
@@ -1,9 +1,21 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Project = {title: String, description: String, tags?: String[]}
+
+class Project {
+
+    constructor({ title, description, tags=[]}) {
+        this.title = title;
+        this.description = description;
+        this.tags = tags;
+    }
+
+    title: String;
+    description: String;
+    tags: String[];
+}
 
 export default (req: NextApiRequest, res: NextApiResponse<Project[]>) => {
     res.status(200).json([
-        {title: 'Portfolio website', description: 'You are looking at it!'},
+        new Project({title: 'Portfolio website', description: 'You are looking at it!'}),
     ]);
 }
