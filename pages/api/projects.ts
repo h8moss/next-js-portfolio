476651bc83a5154758ed9dc130de65745df8ddde
h8m0ss <danarmenta32@gmail.com>
2022-01-30 21:29:12 -0600

    added more projects

--- pages/api/projects.ts
+++ pages/api/projects.ts
@@ -16,6 +16,38 @@ class Project {
 
 export default (req: NextApiRequest, res: NextApiResponse<Project[]>) => {
     res.status(200).json([
-        new Project({title: 'Portfolio website', description: 'You are looking at it!'}),
+          new Project({
+            title: 'Portfolio website', 
+            description: 'You are looking at it!', 
+            tags: [
+                'React',
+                'Next-js',
+                'Tailwind',
+                'Firebase-hosting',
+            ],
+        }),
+       new Project({
+            title: 'Video game deal viewer', 
+            description: 'Some other description', 
+            tags: [
+                'Flutter',
+                'Uses public API',
+                'Android',
+                'Ad-mob',
+            ],
+        }),
+       new Project({
+            title: 'Flutter chat app', 
+            description: 'My last description', 
+            tags: [
+                'Flutter',
+                'Firebase',
+                'Firebase-hosting',
+                'Multi-platform',
+                'PWA',
+                'CRUD',
+            ],
+        }),
+
     ]);
 }
