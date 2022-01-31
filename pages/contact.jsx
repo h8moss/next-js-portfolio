23d218cc86b90f0311420662179a1df0d0d941a4
h8m0ss <danarmenta32@gmail.com>
2022-01-31 17:13:16 -0600

    connected contact form to API

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -2,6 +2,7 @@ import { ErrorMessage, Field, Form, Formik } from "formik";
 import { useEffect, useState } from "react";
 import WithWillExit from "../components/WithWillExit";
 import style from '../styles/Contact.module.css'
+import { server } from '../config';
 
 const _validate = values => {
     let errors = {};
@@ -16,21 +17,65 @@ const _validate = values => {
     } else if (values.message.length > 300) {
         errors.message = 'Your message is too long!';
     }
+    // TODO: add email validation
 
     return errors;
 }
 
+const _send = async (values, { setSubmitting, resetForm, setFieldError }, onSuccess, onFailure) => {
+    const name = values.name;
+    const message = values.message;
+    const email = values.email ? values.email : null;
+
+    const res = await fetch(`${server}/api/contact`, {
+        method: 'POST',
+        body: JSON.stringify({
+            name: name,
+            message: message,
+            email: email,
+        })
+    });
+
+    if (res.status !== 200) {
+        setFieldError('general', 'Something went wrong!');
+        onFailure()
+    } else {
+        resetForm();
+        onSuccess();
+    }
+    setSubmitting(false);
+}
+
 function Skills({ willExit }) {
     const [loaded, setLoaded] = useState(false);
-
     useEffect(() => setTimeout(() => setLoaded(true), 50), []);
+    const [showErrorToast, setShowErrorToast] = useState(false);
+    const [showSuccessToast, setShowSuccessToast] = useState(false);
+
+    useEffect(() => {
+        let time = setTimeout(() => {
+            if (showErrorToast) setShowErrorToast(false);
+            if (showSuccessToast) setShowSuccessToast(false);
+        }, 1000);
+        return () => clearTimeout(time)
+    }, [showErrorToast, showSuccessToast]);
+
+
+    const mySend = (data, formik) => {
+        _send(data, formik, () => { setShowSuccessToast(true) }, () => { setShowErrorToast(true) });
+    }
 
     const showForm = (!willExit && loaded);
-    console.log(showForm);
 
     return (
         <div className="w-screen h-screen p-16 px-60">
             <div className="w-full h-full">
+                <div className={`bg-red-500 w-80 p-8 rounded-xl absolute bottom-5 left-5 transition-transform ${showErrorToast ? 'translate-x-0' : '-translate-x-96'}`}>
+                    <p>Something went wrong! Please try again later</p>
+                </div>
+                <div className={`bg-green-500 w-80 p-8 rounded-xl absolute bottom-5 left-5 transition-transform ${showSuccessToast ? 'translate-x-0' : '-translate-x-96'}`}>
+                    <p>Message successfully received!</p>
+                </div>
                 <Formik
                     initialValues={{
                         name: '',
@@ -38,26 +83,40 @@ function Skills({ willExit }) {
                         message: '',
                     }}
                     validate={_validate}
-                    onSubmit={values => { }}
+                    onSubmit={mySend}
                 >
-                    <Form className="flex flex-col w-full">
-                        <label htmlFor="name" className={`transition-all duration-500  ${(showForm ? '' : 'text-transparent')}`}>What's your name?</label>
+                    {({ isSubmitting, errors }) => (<Form className="flex flex-col w-full">
+                        <label htmlFor="name" className={`transition-all duration-500 ${(showForm ? '' : 'text-transparent')}`}>What's your name?</label>
                         <Field name="name" type="text" className={`${style.fieldTransition} bg-zinc-700 rounded-xl  ${showForm ? ('w-full p-1 ' + style.field) : 'w-0'} ${style.field}`} />
                         {showForm && <ErrorMessage name="name" className={style.errorMessage} component={'p'} />}
 
-                        <label htmlFor="email" className={`transition-all duration-500  ${!willExit ? 'delay-500' : ''}  ${(showForm ? '' : 'text-transparent')}`} >What's your email? (optional)</label>
-                        <Field name="email" type="email" className={`${style.fieldTransition} duration-500 ${!willExit ? 'delay-500' : ''} bg-zinc-700 rounded-xl  ${showForm ? ('w-full p-1 ' + style.field) : 'w-0'} ${style.field}`} />
+                        <label
+                            htmlFor="email"
+                            className={`transition-all duration-500  ${!willExit ? 'delay-500' : ''}  ${(showForm ? '' : 'text-transparent')}`}
+                        >
+                            What's your email?  <span
+                                className={`text-sm transition-all duration-500  ${!willExit ? 'delay-500' : ''} ${(showForm ? 'text-gray-300' : 'text-transparent')}`}
+                            >
+                                (optional)
+                            </span>
+                        </label>
+                        <Field name="email" className={`${style.fieldTransition} duration-500 ${!willExit ? 'delay-500' : ''} bg-zinc-700 rounded-xl  ${showForm ? ('w-full p-1 ' + style.field) : 'w-0'} ${style.field}`} />
                         {showForm && <ErrorMessage name="email" className={style.errorMessage} component={'p'} />}
 
                         <label htmlFor="message" className={`transition-all duration-500 ${!willExit ? 'delay-1000' : ''}  ${(showForm ? '' : 'text-transparent')}`} >What do you want to tell me?</label>
                         <Field name="message" as='textarea' className={`${style.fieldTransition} duration-500 bg-zinc-700 rounded-xl h-64 ${!willExit ? 'delay-1000' : ''} ${showForm ? ('w-full p-1 ' + style.field) : 'w-0'}`} />
                         {showForm && <ErrorMessage name="message" className={style.errorMessage} component={'p'} />}
-
-                        <button type="submit" className={`transition-all duration-300 delay-[1500]  ${(showForm ? 'text-white' : 'text-transparent')}`}>Submit</button>
+                        {
+                            isSubmitting ?
+                                <p>...</p>
+                                : <button type="submit" className={`transition-all duration-300 delay-[1500]  ${(showForm ? 'text-white' : 'text-transparent')}`}>Submit</button>
+                        }
                     </Form>
-                </Formik>
-            </div>
-        </div>
+                    )
+                    }
+                </Formik >
+            </div >
+        </div >
     );
 }
 
