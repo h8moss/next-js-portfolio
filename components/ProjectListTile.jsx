2c4eaa8f0e8ddaace128a1a1eed9f1d33e1bd26a
h8m0ss <danarmenta32@gmail.com>
2022-01-30 21:27:28 -0600

    added tags display on the projects

--- components/ProjectListTile.jsx
+++ components/ProjectListTile.jsx
@@ -1,9 +1,13 @@
-export default function ProjectListTile({ project: { title }, onClick, isSelected }) {
+export default function ProjectListTile({ project: { title, tags }, onClick, isSelected, isVisible }) {
     return (
-        <button className={`border-b-2 ${isSelected ? 'bg-stone-500' : ''} border-gray-400 h-min w-full hover:bg-white transition-all flex`} onClick={onClick}>
-            <p className='m-4'>
+        <button className={`overflow-clip flex-col flex w-full ${isSelected ? 'bg-stone-500' : ''} ${isVisible ? 'h-min border-gray-400 border-b-2' : 'h-0'}   w-full hover:bg-white transition-all duration-300 flex`} onClick={onClick}>
+            {isVisible && [<p className='m-4'>
                 {title}
-            </p>
+            </p>,
+            <div className="flex">
+                {tags.map((tag) => <p className="bg-gray-600 rounded-md text-xs text-gray-300 ml-2 mb-1 p-1 whitespace-nowrap">{tag}</p>)}
+            </div>
+            ]}
         </button>
     );
 }
