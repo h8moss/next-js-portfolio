45dbce728eed8c1e5487f3f61e7fad404ef0ac74
h8m0ss <danarmenta32@gmail.com>
2022-02-01 22:50:31 -0600

    code formatting

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -1,26 +1,26 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { db } from '../../services/firebase/fireship';
-import { collection, addDoc } from 'firebase/firestore';
+import type { NextApiRequest, NextApiResponse } from "next";
+import { db } from "../../services/firebase/firestore";
+import { collection, addDoc } from "firebase/firestore";
 
 const addContactMessage = async (data) => {
-    const col = collection(db, '/contact');
-    return await addDoc(col, data);
-}
+  const col = collection(db, "/contact");
+  return await addDoc(col, data);
+};
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-    if (req.method !== 'POST') {
-        res.status(405).send('Method is not allowed');
-        return
-    }
+  if (req.method !== "POST") {
+    res.status(405).send("Method is not allowed");
+    return;
+  }
 
-    const body = JSON.parse(req.body);
+  const body = JSON.parse(req.body);
 
-    if (!body.message || !body.name || !(body.email || body.email === null)) {
-        res.status(400).send('Missing parameters');
-        return;
-    }
+  if (!body.message || !body.name || !(body.email || body.email === null)) {
+    res.status(400).send("Missing parameters");
+    return;
+  }
 
-    await addContactMessage(body);
+  await addContactMessage(body);
 
-    res.status(200).send('Success');
-}
+  res.status(200).send("Success");
+};
