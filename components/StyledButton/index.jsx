7287cfe2d6b509be1e34f63631bc1e156c494046
h8m0ss <danarmenta32@gmail.com>
2022-02-13 00:19:19 -0600

    added spread props to style button

--- components/StyledButton/index.jsx
+++ components/StyledButton/index.jsx
@@ -1,11 +1,13 @@
 import { motion } from 'framer-motion'
 
-export default function StyledButton({ children, color, onClick }) {
+export default function StyledButton({ children, color, onClick, ...props }) {
     return (
         <motion.button
             initial={{ fontSize: '16px', color: '#fff' }}
             whileHover={{ fontSize: '22px', color: color }}
+            exit={{ scaleX: '0px' }}
             onClick={onClick}
+            {...props}
         >
             {children}
         </ motion.button>
