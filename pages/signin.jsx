8363f4c05b9ab8df1c507d54c249c30feb116810
h8m0ss <danarmenta32@gmail.com>
2022-02-17 19:33:28 -0600

    added transition to signin page

--- pages/signin.jsx
+++ pages/signin.jsx
@@ -1,8 +1,11 @@
 import { ErrorMessage, Field, Form, Formik } from "formik";
+import { AnimatePresence } from "framer-motion";
 import Image from 'next/image';
 import { useRouter } from 'next/router'
 import { useState } from "react";
 
+import Card from '../components/Card';
+import NavBar from "../components/NavBar";
 import ScreenDiv from "../components/ScreenDiv";
 import useUser from "../hooks/useUser";
 import getAuthService from "../services/firebase/auth";
@@ -31,67 +34,78 @@ function Login() {
         }
     }
 
+    let [nextRoute, setNextRoute] = useState(router.pathname);
+
+    let shouldStay = nextRoute === router.pathname;
+
 
     return (
-        <ScreenDiv>
-            <div
-                className="bg-white mx-auto rounded-lg shadow-xl 
-                    text-gray-800 text-center 
-                    w-1/2 h-full flex flex-col"
-            >
-                <Formik
-
-                    initialValues={{
-                        mail: '',
-                        password: '',
-                    }}
-                    validate={() => { }}
-                    onSubmit={() => { }}
+        <>
+            <NavBar
+                onClick={(route) => setNextRoute(route)}
+            />
+            <ScreenDiv>
+                <AnimatePresence
+                    onExitComplete={() => router.push(nextRoute)}
                 >
-                    {() => (
-                        <Form className={style.form}>
-                            <h1 className="text-4xl">{isRegistering ? 'Register' : 'Log in'}</h1>
-                            <label htmlFor="mail" >Email</label>
-                            <Field name="mail" type='mail' />
-                            <ErrorMessage name="mail" component={'p'} />
-                            <label htmlFor="password" >Password</label>
-                            <Field name="password" type='password' />
-                            <ErrorMessage name="password" component={'p'} />
-                            <div className={'transition-all flex flex-col overflow-clip ' + (isRegistering ? 'h-20' : 'h-0')}>
-                                <label htmlFor="password" >Repeat password</label>
-                                <Field name="password2" type='password' />
-                                <ErrorMessage name="password2" component={'p'} />
-                            </div>
-                            <div className="flex flex-row">
-                                <button className={style.loginButton}>Log in</button>
-                                <button
-                                    className={style.createAccount}
-                                    onClick={() => setIsRegistering(!isRegistering)}
-                                >
-                                    {
-                                        isRegistering
-                                            ? "Already have an account? Log in"
-                                            : "Don't have an account? Create one"
-                                    }
+                    {shouldStay &&
+                        <Card>
+                            <Formik
 
-                                </button>
-                            </div>
-                            <p className="text-center text-gray-600 text-sm">or</p>
-                            <button className={style.loginWithGoogle} onClick={googleLogin}>
-                                <div className="my-auto mx-5">
-                                    <Image src='/social_icons/google.png' alt=''
-                                        width={20}
-                                        height={20}
-                                        layout='fixed'
-                                    />
-                                </div>
-                                Log in with google
-                            </button>
-                        </Form>
-                    )}
-                </Formik>
-            </div>
-        </ScreenDiv>
+                                initialValues={{
+                                    mail: '',
+                                    password: '',
+                                }}
+                                validate={() => { }}
+                                onSubmit={() => { }}
+                            >
+                                {() => (
+                                    <Form className={style.form}>
+                                        <h1 className="text-4xl">{isRegistering ? 'Register' : 'Log in'}</h1>
+                                        <label htmlFor="mail" >Email</label>
+                                        <Field name="mail" type='mail' />
+                                        <ErrorMessage name="mail" component={'p'} />
+                                        <label htmlFor="password" >Password</label>
+                                        <Field name="password" type='password' />
+                                        <ErrorMessage name="password" component={'p'} />
+                                        <div className={'transition-all flex flex-col overflow-clip ' + (isRegistering ? 'h-20' : 'h-0')}>
+                                            <label htmlFor="password" >Repeat password</label>
+                                            <Field name="password2" type='password' />
+                                            <ErrorMessage name="password2" component={'p'} />
+                                        </div>
+                                        <div className="flex flex-row">
+                                            <button className={style.loginButton}>Log in</button>
+                                            <button
+                                                className={style.createAccount}
+                                                onClick={() => setIsRegistering(!isRegistering)}
+                                            >
+                                                {
+                                                    isRegistering
+                                                        ? "Already have an account? Log in"
+                                                        : "Don't have an account? Create one"
+                                                }
+
+                                            </button>
+                                        </div>
+                                        <p className="text-center text-gray-600 text-sm">or</p>
+                                        <button className={style.loginWithGoogle} onClick={googleLogin}>
+                                            <div className="my-auto mx-5">
+                                                <Image src='/social_icons/google.png' alt=''
+                                                    width={20}
+                                                    height={20}
+                                                    layout='fixed'
+                                                />
+                                            </div>
+                                            Log in with google
+                                        </button>
+                                    </Form>
+                                )}
+                            </Formik>
+                        </Card>
+                    }
+                </AnimatePresence>
+            </ScreenDiv>
+        </>
     );
 }
 
