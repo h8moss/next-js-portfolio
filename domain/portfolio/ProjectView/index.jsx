344c0532c761eb5eba798ed9cfb78eadea056acd
h8m0ss <danarmenta32@gmail.com>
2022-02-17 19:38:05 -0600

    added description to portfolio

--- domain/portfolio/ProjectView/index.jsx
+++ domain/portfolio/ProjectView/index.jsx
@@ -3,7 +3,7 @@ import { AnimatePresence, motion } from "framer-motion";
 const ProjectView = ({ project }) => {
     return (
         <motion.div
-            className='overflow-clip whitespace-nowrap'
+            className='overflow-clip whitespace-nowrap h-full'
             initial={{ width: '0' }}
             animate={{ width: '50%' }}
             exit={{ width: '0' }}
@@ -12,12 +12,20 @@ const ProjectView = ({ project }) => {
                 exitBeforeEnter
             >
                 <motion.div
+                    className="flex flex-col h-full"
                     key={project.title}
                     initial={{ opacity: 0 }}
                     animate={{ opacity: 1 }}
                     exit={{ opacity: 0 }}
                 >
-                    {project.title}
+                    <h2 className="text-3xl p-5">
+                        {project.title}
+                    </h2>
+                    <div className="flex-grow border-4 p-5 m-2 rounded-xl border-purple-400">
+                        <p>
+                            {project.description}
+                        </p>
+                    </div>
                 </motion.div>
             </AnimatePresence>
         </motion.div>
