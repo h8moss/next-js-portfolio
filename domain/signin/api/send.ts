265bc937ae32320a15993916b0cf5e908d1ab118
h8m0ss <danarmenta32@gmail.com>
2022-02-20 14:33:23 -0600

    added onError callback to submit form

--- domain/signin/api/send.ts
+++ domain/signin/api/send.ts
@@ -6,14 +6,24 @@ import { SigninBody } from "../types";
 const send = async (
   values: SigninBody,
   auth: AuthService,
-  { resetForm, setSubmitting }: FormikHelpers<SigninBody>
+  isRegistering: boolean,
+  { resetForm, setSubmitting }: FormikHelpers<SigninBody>,
+  onError: () => any
 ) => {
-  if (values.isRegistering && values.password === values.password2) {
+  console.log(isRegistering);
+
+  if (isRegistering && values.password === values.password2) {
     try {
       await auth.createAccountWithMail(values.mail, values.password);
-    } catch (e) {}
-  } else if (!values.isRegistering) {
-    await auth.signInWithMail(values.mail, values.password);
+    } catch (e) {
+      onError();
+    }
+  } else if (!isRegistering) {
+    try {
+      await auth.signInWithMail(values.mail, values.password);
+    } catch (e) {
+      onError();
+    }
   }
 
   resetForm();
