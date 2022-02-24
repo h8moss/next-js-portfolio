3e4484d95eee85aaf24b24bc8ff38e4506611be1
h8m0ss <danarmenta32@gmail.com>
2022-02-24 15:45:06 -0600

    made navbar resonsive

--- components/NavBar/index.jsx
+++ components/NavBar/index.jsx
@@ -1,54 +1,51 @@
-import { useContext } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
+import { useState } from 'react';
+import { FiMenu } from 'react-icons/fi';
 
-import AuthContext from '../../context/auth';
-import useAuth from '../../hooks/useAuth';
 import useUser from '../../hooks/useUser';
 import StyledButton from '../StyledButton';
+import Buttons from './Buttons';
 import UserButton from './UserButton';
 
 export default function NavBar({ onClick }) {
-    const auth = useContext(AuthContext);
-
-    let user = useUser();
+    const [showMenu, setShowMenu] = useState(false);
 
     return (
         <>
-            <div className='flex flex-row w-full justify-evenly absolute z-[95] py-3 bg-zinc-900 shadow-xl'>
-                <StyledButton
-                    color='#f00'
-                    onClick={() => onClick('/')}
-                >
-                    About
-                </StyledButton>
-                <StyledButton
-                    color='#3070fa'
-                    onClick={() => onClick('/portfolio')}
-                >
-                    Portfolio
-                </StyledButton>
-                <StyledButton
-                    color='#aa85ff'
-                    onClick={() => onClick('/contact')}
-                >
-                    Contact
-                </StyledButton>
-                <StyledButton
-                    color='#34ff94'
-                    onClick={() => onClick('/blog')}
-                >
-                    Blog
-                </StyledButton>
-                {user === null
-                    ? <StyledButton
-                        color='#ff0'
-                        onClick={() => onClick('/signin')}
-                    >
-                        Sign in
-                    </StyledButton>
-                    : <UserButton />
-                }
-
+            <div className='  absolute z-[95] py-3 bg-zinc-900 shadow-xl w-full'>
+                <div className='md:scale-100 scale-0 flex flex-row w-full justify-evenly'>
+                    <Buttons onClick={onClick} />
+                </div>
+                <motion.button
+                    onClick={() => setShowMenu((state) => !state)} className='md:scale-0 scale-100 absolute top-0 left-0 m-3'>
+                    <FiMenu height={40} width={40} />
+                </motion.button>
             </div>
+
+            <AnimatePresence>
+                {showMenu &&
+                    <div className='bg-opacity-50 fixed top-0 left-0 w-screen h-screen z-[99]'
+                        onClick={() => setShowMenu(false)}
+                    >
+                        <motion.div
+                            onClick={(e) => e.stopPropagation()}
+                            className='flex flex-col bg-slate-500 w-36 justify-center'
+                            initial={{ x: '-200px' }}
+                            animate={{ x: '0px' }}
+                            exit={{ x: '-200px' }}
+                        >
+                            <Buttons
+                                buttonSize={24}
+                                buttonClass='py-3'
+                                onClick={(route) => {
+                                    setShowMenu(false);
+                                    onClick(route);
+                                }}
+                            />
+                        </motion.div>
+                    </div>
+                }
+            </AnimatePresence>
         </>
     );
 };
