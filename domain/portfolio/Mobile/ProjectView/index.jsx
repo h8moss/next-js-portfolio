8b105cde9d6d937d9b6450607ad62f403983af6e
h8m0ss <danarmenta32@gmail.com>
2022-02-28 12:26:19 -0600

    worked the height of the project view

--- domain/portfolio/Mobile/ProjectView/index.jsx
+++ domain/portfolio/Mobile/ProjectView/index.jsx
@@ -1,32 +1,35 @@
 import { AnimatePresence, motion } from "framer-motion";
+import { useEffect, useState } from "react";
 
 import ProjectDescription from "../../ProjectDescription";
 
+const bgVariants = {
+    'opacity-0': { backgroundColor: 'rgba(0,0,0,0)' },
+    'opacity-1': { backgroundColor: 'rgba(0,0,0,0.2)' },
+};
 
 const MobileProjectView = ({ project, onExit }) => {
 
-    let showProject = project !== null;
-
     return (
-        <AnimatePresence>
-            {showProject &&
-                <div
-                    className="bg-black bg-opacity-10 top-0 left-0 static w-screen h-screen flex"
-                    onClick={onExit}
+        <motion.div
+            className="bg-black bg-opacity-20 top-0 left-0 absolute w-screen h-screen flex z-[99]"
+            onClick={onExit}
+            variants={bgVariants}
+            initial='opacity-0'
+            animate='opacity-1'
+            exit='opacity-0'
+        >
+            <div onClick={(e) => e.stopPropagation()} className='h-min w-full mx-4 my-auto'>
+                <motion.div
+                    className="bg-white text-black rounded-xl p-1"
+                    initial={{ y: '-100vh' }}
+                    animate={{ y: '0vh' }}
+                    exit={{ y: '100vh' }}
                 >
-                    <div onClick={(e) => e.stopPropagation()} className='h-full w-full m-8'>
-                        <motion.div
-                            className="h-full bg-white text-black rounded-sm"
-                            initial={{ y: '-100vh' }}
-                            animate={{ y: '0vh' }}
-                            exit={{ y: '100vh' }}
-                        >
-                            <ProjectDescription project={project} />
-                        </motion.div>
-                    </div>
-                </div>
-            }
-        </AnimatePresence>
+                    <ProjectDescription project={project} />
+                </motion.div>
+            </div>
+        </motion.div>
     )
 }
 
