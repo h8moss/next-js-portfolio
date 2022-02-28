b20abd4858fa9571b1d0e599206956e6d3f23e7f
h8m0ss <danarmenta32@gmail.com>
2022-02-28 14:39:50 -0600

    fixed issue with window object

--- hooks/useWindowSize.js
+++ hooks/useWindowSize.js
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 
 const useWindowSize = () => {
-    const [currentWidth, setCurrentWidth] = useState(window.innerWidth);
-    const [currentHeight, setCurrentHeight] = useState(window.innerHeight);
+    const [currentWidth, setCurrentWidth] = useState(0);
+    const [currentHeight, setCurrentHeight] = useState(0);
 
     useEffect(() => {
         const onChange = () => {
@@ -10,10 +10,11 @@ const useWindowSize = () => {
             setCurrentHeight(window.innerHeight);
         }
 
+        onChange();
         window.addEventListener('resize', onChange);
 
         return () => window.removeEventListener('resize', onChange);
-    });
+    }, []);
 
     return [currentWidth, currentHeight];
 }
