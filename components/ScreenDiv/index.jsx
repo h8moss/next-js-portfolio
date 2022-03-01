24e91f6198dadb1f7910fbb8a021c0a45bdfdcc6
h8m0ss <danarmenta32@gmail.com>
2022-03-01 09:51:00 -0600

    made signin form responsive

--- components/ScreenDiv/index.jsx
+++ components/ScreenDiv/index.jsx
@@ -1,7 +1,7 @@
 const ScreenDiv = ({ children, className, ...restProps }) => {
     return (
         <div
-            className={"h-screen w-screen overflow-clip p-10 md:p-20 sm:p-1 " + className}
+            className={"h-screen w-screen overflow-clip md:p-20 p-3 py-10 " + className}
             {...restProps}
         >
             {children}
