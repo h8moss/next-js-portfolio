becb1af4a5f6e6b4a381cfb2f096bec182492232
h8m0ss <danarmenta32@gmail.com>
2022-03-02 23:18:34 -0600

    added index i18n file

---
+++ domain/index/i18n/index.ts
@@ -0,0 +1,38 @@
+import { i18nLangs } from "./types";
+
+const i18n: i18nLangs = {
+  es: {
+    title: "Me llamo Daniel Armenta",
+    lookAtSocials: "Abre mis redes sociales",
+    description: [
+      "Soy un desarrollador con experiencia en ",
+      "Flutter",
+      ", ",
+      "Python",
+      ", ",
+      "C++",
+      ", ",
+      "Javascript",
+      " y ",
+      "más",
+    ],
+  },
+  en: {
+    title: "My name is Daniel Armenta",
+    lookAtSocials: "Take a look at my socials",
+    description: [
+      "I'm a developer with skills in ",
+      "Flutter",
+      ", ",
+      "Python",
+      ", ",
+      "C++",
+      ", ",
+      "Javascript",
+      " and ",
+      "more",
+    ],
+  },
+};
+
+export default i18n;
