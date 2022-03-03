768169c74426d08dcf90435d8109185154e7fd1f
h8m0ss <danarmenta32@gmail.com>
2022-03-02 23:18:44 -0600

    added export of i18n file on index

--- domain/index/index.js
+++ domain/index/index.js
@@ -1,4 +1,5 @@
 import ButtonLink from './ButtonLink';
+import i18n from './i18n';
 import MainTextWriter from './MainTextWriter';
 
-export { ButtonLink, MainTextWriter }
+export { ButtonLink, i18n, MainTextWriter }
