82ad15455b6d68eb5f2e2970e04f248b478978b8
h8m0ss <danarmenta32@gmail.com>
2022-03-23 12:58:14 -0600

    Added i18n descriptions to every page

--- domain/portfolio/i18n/index.ts
+++ domain/portfolio/i18n/index.ts
@@ -3,9 +3,13 @@ import { I18nBody } from "./types";
 const i18n: I18nBody = {
   es: {
     title: "Portafolio",
+    metaDescription:
+      "Aquí puedes ver y leer sobre todos los proyectos en los que he trabajado",
   },
   en: {
     title: "Portfolio",
+    metaDescription:
+      "Here you can read about all of the projects I've ever worked on",
   },
 };
 
--- domain/portfolio/i18n/types.ts
+++ domain/portfolio/i18n/types.ts
@@ -2,6 +2,7 @@ import { I18n } from "../../../types";
 
 export type LanguageBody = {
   title: string;
+  metaDescription: string;
 };
 
 export type I18nBody = I18n<LanguageBody>;
