44d750745828c993d84fc7ccaa15f1729363ed3e
h8m0ss <danarmenta32@gmail.com>
2022-03-23 15:48:23 -0600

    fixed import sorting

--- components/NavBar/index.jsx
+++ components/NavBar/index.jsx
@@ -1,6 +1,6 @@
 import { AnimatePresence, motion } from 'framer-motion';
-import Link from 'next/link';
 import Image from 'next/image';
+import Link from 'next/link';
 import { useState } from 'react';
 import { FiMenu } from 'react-icons/fi';
 
