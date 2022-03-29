fe656d677b314bd04f0b46df52fcdc7594e78d11
h8m0ss <danarmenta32@gmail.com>
2022-03-29 14:05:22 -0600

    added custom class to Button

--- components/Button/Button.jsx
+++ components/Button/Button.jsx
@@ -1,9 +1,10 @@
-const Button = ({ onClick, children, color }) => {
+const Button = ({ onClick, children, color, className, ...props }) => {
     return (
         <button
             onClick={onClick}
-            className='p-3 hover:scale-105'
+            className={'p-3 hover:scale-105 ' + className}
             style={{ color }}
+            {...props}
         >
             {children}
         </button>
