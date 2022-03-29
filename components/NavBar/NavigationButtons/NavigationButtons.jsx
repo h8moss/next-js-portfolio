623cb464c5f331f255495c511e1064577a2d5597
h8m0ss <danarmenta32@gmail.com>
2022-03-29 14:06:11 -0600

    made highlight a single color

--- components/NavBar/NavigationButtons/NavigationButtons.jsx
+++ components/NavBar/NavigationButtons/NavigationButtons.jsx
@@ -3,10 +3,10 @@ import RouteButton from "../RouteButton";
 const NavigationButtons = ({ onClick }) => {
     return (
         <>
-            <RouteButton highlightColor='#0f0' onClick={onClick} route={'/'}>Home</RouteButton>
-            <RouteButton highlightColor='#a0f' onClick={onClick} route={'/portfolio'}>Portfolio</RouteButton>
-            <RouteButton highlightColor='#f00' onClick={onClick} route={'/contact'}>Contact</RouteButton>
-            <RouteButton highlightColor='#ff0' onClick={onClick} route={'/blog'}>Blog</RouteButton>
+            <RouteButton onClick={onClick} route={'/'}>Home</RouteButton>
+            <RouteButton onClick={onClick} route={'/portfolio'}>Portfolio</RouteButton>
+            <RouteButton onClick={onClick} route={'/contact'}>Contact</RouteButton>
+            <RouteButton onClick={onClick} route={'/blog'}>Blog</RouteButton>
         </>
     );
 }
