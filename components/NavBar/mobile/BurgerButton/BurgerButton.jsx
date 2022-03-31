419dda5f798ddd6cc88e9e08720be0b5889542ab
h8m0ss <danarmenta32@gmail.com>
2022-03-30 19:22:24 -0600

    made burger bigger on smaller devices

--- components/NavBar/mobile/BurgerButton/BurgerButton.jsx
+++ components/NavBar/mobile/BurgerButton/BurgerButton.jsx
@@ -4,11 +4,9 @@ import useTailwindSize from '../../../../hooks/useTailwindSize';
 
 const getSize = ({ isSm, isMd, isLg, isXl, isXl2 }) => {
     if (isXl2) return 100;
-    if (isXl) return 80;
     if (isLg) return 80;
-    if (isMd) return 70;
-    if (isSm) return 70;
-    return 30;
+    if (isSm) return 45;
+    return 55;
 }
 
 const BurgerButton = ({ onClick }) => {
