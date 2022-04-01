d6f8cbf055a9326647b74a1682df811112d74357
h8m0ss <danarmenta32@gmail.com>
2022-04-01 13:29:40 -0600

    added basic about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,8 +1,21 @@
+import { AnimatePresence } from "framer-motion";
+import { useRouter } from "next/router";
+import { useState } from "react";
+
+import NavBar from "../components/NavBar";
+
 const About = () => {
+  const router = useRouter();
+
+  const [nextRoute, setNextRoute] = useState(router.pathname);
+
+  const shouldStay = nextRoute == router.pathname;
+
   return (
-    <div>
-      <p>WIP still not done yet</p>
-    </div>
+    <>
+      <NavBar onClick={(route) => setNextRoute(route)} />
+      <AnimatePresence>{shouldStay && <div></div>}</AnimatePresence>
+    </>
   );
 };
 
