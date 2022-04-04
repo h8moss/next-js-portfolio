bd5e8ac458679cdcde450c71f3ea82b6385b27ed
h8m0ss <danarmenta32@gmail.com>
2022-04-04 14:55:21 -0500

    added basic i18n types to about

---
+++ domain/about/i18n/index.ts
@@ -0,0 +1,8 @@
+import { I18nLangs } from "./types";
+
+const i18n: I18nLangs = {
+  en: {},
+  es: {},
+};
+
+export default i18n;
---
+++ domain/about/i18n/types.ts
@@ -0,0 +1,5 @@
+import { I18n } from "../../../types";
+
+export type LanguageBody = {};
+
+export type I18nLangs = I18n<LanguageBody>;
