7e50c32a999fd86e95eb68383848d79150c909c3
h8m0ss <danarmenta32@gmail.com>
2022-04-25 15:18:57 -0500

    added i18n to navbar

---
+++ components/NavBar/NavigationButtons/i18n/index.ts
@@ -0,0 +1,18 @@
+import { I18nBody } from "./types";
+
+const i18n: I18nBody = {
+  en: {
+    about: "About",
+    blog: "Blog",
+    contact: "Contact",
+    portfolio: "Portfolio",
+  },
+  es: {
+    about: "Sobre mí",
+    blog: "Blog",
+    contact: "Contacto",
+    portfolio: "Portafolio",
+  },
+};
+
+export default i18n;
