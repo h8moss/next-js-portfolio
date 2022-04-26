7164a8cbf57a8f1ffec38092779e1a15873d2225
h8m0ss <danarmenta32@gmail.com>
2022-04-26 08:22:01 -0500

    added biography component

---
+++ domain/about/Biography/Biography.tsx
@@ -0,0 +1,29 @@
+import { PropsWithChildren, ReactNode } from "react";
+
+import useLocale from "../../../hooks/useLocale";
+import { ExtendedDateFormat } from "../../../services/dateOperations/types";
+import { Locale } from "../../../types";
+import BiographyEn from "./BiographyEn/BiographyEn";
+import BiographyEs from "./BiographyEs/BiographyEs";
+
+export interface Props {
+  format: ExtendedDateFormat;
+}
+
+const getLocaleBiography = (locale: Locale, props: Props): ReactNode => {
+  switch (locale) {
+    case "en":
+      return <BiographyEn {...props} />;
+    case "es":
+      return <BiographyEs {...props} />;
+    default:
+      return "UNIMPLEMENTED LOCALE, PLEASE CONTACT DEVELOPER";
+  }
+};
+
+const Biography = (props: Props) => {
+  const locale = useLocale();
+
+  return <>{getLocaleBiography(locale, props)}</>;
+};
+export default Biography;
