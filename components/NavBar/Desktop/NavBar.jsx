86d3a67ccc4258ce982b173cc9ba475df78c60cb
h8m0ss <danarmenta32@gmail.com>
2022-04-26 20:22:57 -0500

    gave navbar z index of 99

--- components/NavBar/Desktop/NavBar.jsx
+++ components/NavBar/Desktop/NavBar.jsx
@@ -3,7 +3,7 @@ import NavigationButtons from '../NavigationButtons';
 
 const NavBar = ({ onClick }) => {
     return (
-        <div className="flex flex-row bg-black shadow-2xl top-0 sticky w-full">
+        <div className="flex flex-row bg-black shadow-2xl top-0 sticky w-full z-[99]">
             <div className='flex-grow flex justify-center'>
                 <button className='m-auto' onClick={() => onClick('/')}>
                     <Logo />
