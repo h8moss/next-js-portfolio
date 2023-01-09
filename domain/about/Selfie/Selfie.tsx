dfa25f0dd823270d580f9738b5f4791786ced5f5
h8m0ss <danarmenta32@gmail.com>
2023-01-08 21:19:40 -0600

    added selfie animation

--- domain/about/Selfie/Selfie.tsx
+++ domain/about/Selfie/Selfie.tsx
@@ -1,17 +1,21 @@
 import { motion } from "framer-motion";
 import Image from "next/image";
+import { useState } from "react";
 
 interface Props {
   description: string;
 }
 
 const Selfie = ({ description }: Props) => {
+  const [beenRendered, setBeenRendered] = useState(false);
+
   return (
     <motion.div
       className="rounded-full my-3 w-80 h-80 mx-auto overflow-clip shadow-2xl"
-      initial={{ scale: 0 }}
-      animate={{ scale: 1 }}
       exit={{ scale: 0 }}
+      animate={{ scale: beenRendered ? 1 : 0, rotate: beenRendered ? 0 : 90 }}
+      whileInView={{ scale: 1, rotate: 0 }}
+      onViewportEnter={() => setBeenRendered(true)}
     >
       <Image
         className="rotate-[39deg]"
