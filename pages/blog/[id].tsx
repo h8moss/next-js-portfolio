29c1d6c821a1e66e5a47cb6deb6a6383f6edc534
h8m0ss <danarmenta32@gmail.com>
2023-04-28 08:19:14 -0600

    FEAT: Made blog page use storage images

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -1,5 +1,6 @@
+import { getDownloadURL, ref } from "firebase/storage";
 import { AnimatePresence, motion } from "framer-motion";
-import { GetServerSideProps, GetStaticPaths, GetStaticProps } from "next";
+import { GetServerSideProps } from "next";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import { useState } from "react";
@@ -7,8 +8,10 @@ import { useState } from "react";
 import BlogViewer from "../../components/BlogViewer";
 import NavBar from "../../components/NavBar";
 import ScrollToTop from "../../domain/about/ScrollToTop";
-import { getBlog, getBlogs } from "../../domain/blog/api";
-import { BlogPost, Locale } from "../../types";
+import { getBlog } from "../../domain/blog/api";
+import useLocale from "../../hooks/useLocale";
+import { storage } from "../../services/firebase";
+import { BlogPost } from "../../types";
 
 interface Props {
   post: BlogPost;
@@ -17,6 +20,7 @@ interface Props {
 function Blog({ post }: Props) {
   const router = useRouter();
   const [nextRoute, setNextRoute] = useState(router.pathname);
+  const locale = useLocale();
 
   const shouldStay = nextRoute === router.pathname;
 
@@ -31,7 +35,7 @@ function Blog({ post }: Props) {
           {shouldStay && (
             <>
               <motion.div
-                className="md:w-1/2 w-[95%] m-auto mt-10"
+                className="md:w-1/2 w-[95%] m-auto mt-10 min-h-screen"
                 initial={{
                   x: "100vw",
                 }}
@@ -42,7 +46,14 @@ function Blog({ post }: Props) {
                   x: "-100vw",
                 }}
               >
-                <BlogViewer post={post} />
+                <BlogViewer
+                  post={post}
+                  handleStorageImage={async (name) => {
+                    return await getDownloadURL(
+                      ref(storage, `blog-posts-${locale}/${post.id}/${name}`)
+                    );
+                  }}
+                />
               </motion.div>
 
               <ScrollToTop show={shouldStay} />
