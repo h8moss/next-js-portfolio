ab92ffc263bbf57d8d30e9621e18da55df14e1ee
h8m0ss <danarmenta32@gmail.com>
2023-05-09 10:12:56 -0600

    FEAT: Added a lint viewer

---
+++ domain/admin/create-blog-post/LintViewer/LintViewer.tsx
@@ -0,0 +1,21 @@
+import { LintError } from "remark-linter/lib";
+
+import style from "./LintViewer.module.css";
+
+interface Props {
+  lints: LintError[];
+}
+
+const LintViewer = ({ lints }: Props) => {
+  return (
+    <ul className={style.lint}>
+      {lints.map((lint) => (
+        <li key={lint.name + lint.row + lint.column}>
+          {`${lint.name}:${lint.reason} @${lint.row}:${lint.column}`}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default LintViewer;
